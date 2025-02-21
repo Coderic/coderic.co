58e18daf1f53b73bccf944b2c2d2515c52b84ab8
Neftalí Yagua <neftali.yagua@gmail.com>
2025-02-21 03:25:38 -0500

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,35 @@
+var webAuth = new auth0.WebAuth({
+    clientID: 'ZEZjUDVOSfUoDIN1cKYLkohGybyyfiBE',
+    domain: 'auth.coderic.org',
+    audience: `https://coderic.eu.auth0.com/api/v2/`,
+    scope: 'openid profile email',
+    redirectUri: 'https://coderic.co/',
+    responseType: 'token id_token'
+});
+
+$(document).ready(function() {
+    webAuth.checkSession(
+    {
+        audience: 'https://coderic.eu.auth0.com/api/v2/',
+        scope: 'openid profile email',
+    },
+    function(err, result) {
+      console.dir(result);
+      if (err || !result || !result.accessToken) {
+        $(".guest").show();
+        $(".authenticated").hide();
+      } else {
+        webAuth.client.userInfo(result.accessToken, function(err, user) {
+          if (!err && user) {
+            $("#username").text(user.name).show();
+          }
+        });
+        $(".guest").hide();
+        $(".authenticated").show();
+      }
+    });
+  });
+
+logout = () => webAuth.logout({
+      returnTo: 'https://' +window.location.hostname + '/'
+});
